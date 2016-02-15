939daa049add89b26e056461d4013c1b40d4cbfb
srs1212 <info@greenspoon.us>
2016-02-15 10:43:38 -0700

    2nd commit

---
+++ myCalculator.js
@@ -0,0 +1,9 @@
+var Calculator = require('./calculator');
+
+var myCalculatorSum = new Calculator();
+myCalculatorSum.sum(10,3);
+
+var myCalculatorSub = new Calculator();
+myCalculatorSub.subtract(10,3);
+
+
